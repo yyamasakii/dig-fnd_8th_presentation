6a27f5aa2f41d7c16e9104a98ac752b36ac5e232
yyamasakii <108397738+yyamasakii@users.noreply.github.com>
2022-08-05 15:04:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const imgPathArray = [
     "bot_img/bot_img6.png",
     "bot_img/bot_img7.png",
     "bot_img/bot_img8.png",
+    "bot_img/nightmare_day03_dig-comparisons-intro.png"
 ];
 
 // 0-2) リンクのURLが入った配列を準備
@@ -23,7 +24,8 @@ const urlArray = [
     "https://toyotatimes.jp/series/toyoda_words/005.html",
     "https://toyotatimes.jp/series/toyoda_words/006.html",
     "https://toyotatimes.jp/series/toyoda_words/007.html",
-    "https://toyotatimes.jp/series/toyoda_words/008.html"
+    "https://toyotatimes.jp/series/toyoda_words/008.html",
+    "https://yyamasakii.github.io/dig-fnd_8th_presentation/"
 ]
 
 // 0-3) リンクのURLが入った配列を準備
@@ -35,7 +37,8 @@ const numberArray = [
     "#05「持続的成長」",
     "#06「意志ある情熱と行動」",
     "#07「負け嫌い」",
-    "#08「自分以外の誰かのために」"
+    "#08「自分以外の誰かのために」",
+    "ナイトメア問題（Week 1 - Wednesday 2022/07/13 dig-comparisons-intro)"
 ]
 
 // ガチャクリックで動く関数
